a8bd57a1ee6e2b8eb158c6b36dbd0ececc0109b3
Agustina Sosa <agustinasf85@gmail.com>
2019-05-01 17:58:59 -0400

    Created TodoForm component and finished App.js (something is not right, working on it)

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TodoForm(props) {
+    return (
+        <form onSubmit = {props.addNewItem}>
+            <input 
+                value = {porps.task} 
+                type= "text" 
+                name= "task" 
+                placeholder= "Todo task" 
+                onChange= {props.changeHandler}/>
+
+            <button type = "submit">Add Item</button>
+            <button type = "submit">Erase Completed</button>    
+        </form> 
+    );
+};
+
+export default TodoForm;
