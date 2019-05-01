0b3f17f9aa91b657b2917bef707f2c4d2f41c59d
Agustina Sosa <agustinasf85@gmail.com>
2019-05-01 16:15:51 -0400

    created array for todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+// import TodoList from './components/TodoComponents/TodoList';
+// import TodoForm from './components/TodoComponents/TodoForm';
+
+const todoItems = [
+  {task: "Lorem ipsum dolor sit amet 1", id: 1, comppleted: false},
+  {task: "Lorem ipsum dolor sit amet 2", id: 2, comppleted: false},
+  {task: "Lorem ipsum dolor sit amet 3", id: 3, comppleted: false},
+  {task: "Lorem ipsum dolor sit amet 4", id: 4, comppleted: false},
+  {task: "Lorem ipsum dolor sit amet 5", id: 5, comppleted: false}
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
